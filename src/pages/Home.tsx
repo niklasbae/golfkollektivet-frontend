e900c11a9826bd695767dd4736ed6a6c32334883
Niklas Bae Pedersen <niklas.pedersen@banenor.no>
2025-03-27 12:43:54 +0100

    favicon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="container" style={{ textAlign: 'center', marginTop: '5rem' }}>
       <h1>Golfkollektivet</h1>
-      <p>Vi lanserer snart! Følg med på <a href="https://www.instagram.com/">Instagram</a></p>
+      <p>Vi lanserer snart! Følg med på <a href="https://www.instagram.com/golfkollektivet">Instagram</a></p>
     </div>
   );
 };
