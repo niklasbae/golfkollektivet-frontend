3a5c5175f0f0586b94a63984579ecffd78dff0e6
Niklas Bae Pedersen <niklas.pedersen@banenor.no>
2025-03-27 12:23:30 +0100

    home page fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="container">
       <h1>Velkommen til Golfkollektivet</h1>
-      <p>Dette er hjemmesiden for moderne, sosial golf for 20–40-åringer i Norge.</p>
+      <p>En uformell møteplass med fokus på fellesskap, humor og gode golfopplevelser</p>
     </div>
   );
 };
