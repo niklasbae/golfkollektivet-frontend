f0498c0eb31ff4fe6e83f708cf468cce53d83f8d
Niklas Bae Pedersen <niklas.pedersen@banenor.no>
2025-03-27 12:36:03 +0100

    cleaner coming soon

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './../components/Navbar';
-import Footer from './../components/Footer';
 import Home from './../pages/Home';
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Navbar />
-      <main style={{ paddingTop: '4rem' }}>
+      <main>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
-      <Footer />
     </Router>
   );
 };
